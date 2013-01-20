72f94f7ace73c4d6f26c49dee9e724205bb4fdeb
Robin Yang <yyynyc@gmail.com>
2013-01-20 12:29:31 -0500

    finished setting up user model and tests

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ group :development, :test do
   gem 'guard-rspec', '1.2.1'
   gem 'guard-spork', '1.2.0'  
   gem 'spork', '0.9.2'
-  gem 'annotate'
+  gem 'annotate', '2.5.0'
 end
 
 # Gems used only for assets and not required
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 ShoolooV2::Application.routes.draw do
+
     root to: "static_pages#home"
  
   match '/about', to: 'static_pages#about'
   match '/help', to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'
-  match 'profile', to: 'users#show'
+  match '/signup', to: 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 

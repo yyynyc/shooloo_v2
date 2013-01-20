9cd9a62cc47d55a6140e8b7605feb633f1ee5dbf
Robin Yang <yyynyc@gmail.com>
2013-01-20 08:19:48 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShoolooV2::Application.config.secret_token = 'db37557129650fb35b8146eb26bcec181efb2763113408aa842db4c7f7cbd3d2ad3cc3abcc9f9027e5c87d0b4d5a3c879da5b5e9acf7ef1a2e8fa4b3fa6d1dc2'

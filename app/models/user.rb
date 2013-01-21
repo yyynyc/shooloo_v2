cf735b59479c465e92d3062512eb00761cff4b4b
Robin Yang <yyynyc@gmail.com>
2013-01-21 17:28:22 -0500

    finished updating users

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :email_confirmation, :name, :password, :password_confirmation
   has_secure_password
 
-  before_save { self.email.downcase! }
-  before_save { self.name.capitalize! }
+  before_save { self.email.downcase! } 
   before_save :create_remember_token
 
 
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,9 +1,18 @@
-	# Returns the full title on a per-page basis.
-	def full_title(page_title)
-		base_title = "Shooloo Games"
-		if page_title.empty?
-			base_title
-		else
-			"#{base_title} | #{page_title}" 
-		end
+# Returns the full title on a per-page basis.
+def full_title(page_title)
+	base_title = "Shooloo Games"
+	if page_title.empty?
+		base_title
+	else
+		"#{base_title} | #{page_title}" 
 	end
+end
+
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end

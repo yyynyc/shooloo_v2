9c6a0597349d7f5bf5df27937538db81d20f0c3f
Robin Yang <yyynyc@gmail.com>
2013-01-24 16:53:17 -0500

    finished following post feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,8 +39,7 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   def feed
-    # This is preliminary. See "Following users" for the full implementation.
-    Post.where("user_id = ?", id)
+    Post.from_users_followed_by(self)
   end
 
   def following?(other_user)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -179,10 +179,21 @@ describe User do
       let(:unfollowed_post) do
         FactoryGirl.create(:post, user: FactoryGirl.create(:user))
       end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.posts.create!(question: "Lorem ipsum", answer: "Lorem ipsum", grade: "5") }
+      end
 
       its(:feed) { should include(newer_post) }
       its(:feed) { should include(older_post) }
       its(:feed) { should_not include(unfollowed_post) }
+      its(:feed) do
+        followed_user.posts.each do |post|
+          should include(post)
+        end
+      end
     end
   end
 

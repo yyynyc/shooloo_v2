151b6b0a41f620edbd5b67bd7094bd918d55ccc9
Robin Yang <yyynyc@gmail.com>
2013-02-25 13:02:15 -0500

    fixed nginx unicorn files

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,6 +4,7 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog.sock"
+listen "/tmp/unicorn.shooloo_v2.sock"
+listen "127.0.0.1:3000"
 worker_processes 2
 timeout 30

b959b804219a68adae47ba369e05a709ef3b0224
Robin Yang <yyynyc@gmail.com>
2013-04-05 13:33:50 -0400

    added privacy and terms of use

--- config/routes.rb
+++ config/routes.rb
@@ -41,6 +41,8 @@ resources :alarms
   match '/about', to: 'static_pages#about'
   match '/help', to: 'static_pages#help'
   match '/contact', to: 'static_pages#contact'
+  match '/terms', to: 'static_pages#terms'
+  match '/privacy', to: 'static_pages#privacy'
   match '/signup', to: 'users#new'
   match '/signin', to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete 

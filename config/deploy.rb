e89be2045f23774e997251685a72895c0a085ff5
Robin Yang <yyynyc@gmail.com>
2013-04-08 18:43:03 -0400

    set up deploy automatic cleanup for 3  versions in capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,7 @@ set :branch, "master"
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
+set :keep_releases, 3
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
 
 namespace :deploy do

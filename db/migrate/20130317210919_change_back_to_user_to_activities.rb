80ca752ed36aead1b34495c7dd7dbd062f7a1f35
Robin Yang <yyynyc@gmail.com>
2013-04-09 18:47:33 -0400

    changed to youtube iframe

--- db/migrate/20130317210919_change_back_to_user_to_activities.rb
+++
@@ -1,9 +0,0 @@
-class ChangeBackToUserToActivities < ActiveRecord::Migration
-  def up
-  	rename_column :activities, :initiator_id, :user_id
-  end
-
-  def down
-  	rename_column :activities, :user_id, :initiator_id	
-  end
-end

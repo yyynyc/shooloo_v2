cd7f01750a66278e82569ee939a07d44ce996521
Robin Yang <yyynyc@gmail.com>
2013-04-12 18:30:31 -0400

    tried to delete old activity records, not sure if it will work

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,4 +3,8 @@ class Activity < ActiveRecord::Base
 	attr_accessible :action, :trackable, :recipient_id, :user_id
   	belongs_to :trackable, polymorphic: true
   	belongs_to :user
+
+  	def destroy_old_data
+  		Activity.delete_all(['created_at > ?', 30.days.ago])
+  	end
 end

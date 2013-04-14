9a959aba7edb603b3edace3deff40f7bce2b2f99
Robin Yang <yyynyc@gmail.com>
2013-04-14 13:54:14 -0400

    took whenever out of capistrano

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,8 +3,4 @@ class ActivitiesController < ApplicationController
     	@activities = Activity.paginate(page: params[:page], per_page: 30, 
     		order: 'created_at DESC')
   	end
-
-  	def recipient
-  		User.find(activity.recipient_id)
-  	end
 end

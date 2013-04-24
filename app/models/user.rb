5c8ac4e2de759cb935aceef0d3308351080c2396
Robin Yang <yyynyc@gmail.com>
2013-04-24 12:02:28 -0400

    changed user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -87,11 +87,11 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
   	uniqueness: { case_sensitive: false}
-  validates_confirmation_of :email
+  validates_confirmation_of :email, on: :create
   
 
-  validates :password, presence: true, length: {minimum: 6}
-  validates :password_confirmation, presence: true
+  validates :password, presence: true, length: {minimum: 6}, on: :create
+  validates_confirmation_of :password_confirmation
   validates :privacy, presence: true
   validates :rules, presence: true
 

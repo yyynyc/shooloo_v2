863da553574b50ecbe9f97d0540b3edc4d7181f7
Robin Yang <yyynyc@gmail.com>
2013-06-07 09:12:52 -0400

    changed redirect path after a user is deleted by admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,7 +57,7 @@ class UsersController < ApplicationController
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted."
-    redirect_to users_url
+    redirect_to users_path
   end
 
   def following

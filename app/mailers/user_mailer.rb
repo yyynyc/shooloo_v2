f12b2132a100fc6263f6f8d953ee43656811f02f
Robin Yang <yyynyc@gmail.com>
2013-06-17 15:54:35 -0400

    modified default from: in user_mailer.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "Shooloo@shooloo.org"
+  default from: "Shooloo@fun.shooloo.org"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:

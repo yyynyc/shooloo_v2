28c1cfe2bd1b051753f095670025c1a1041a010c
Robin Yang <yyynyc@gmail.com>
2013-06-21 19:29:16 -0400

    changed default from: in contact.rb

--- app/mailers/contact.rb
+++ app/mailers/contact.rb
@@ -1,9 +1,9 @@
 class Contact < ActionMailer::Base
-  
+  default from: "Shooloo@fun.shooloo.org"
+
   def notification(message)
     @message = message
     mail 	to: "ryang@prosperityprana.com", 
-    		from: message.email,
     		subject: "[Shooloo.org] #{message.subject}"
   end
 end

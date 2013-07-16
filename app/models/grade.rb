00c912142822bf3d9265d911d56e01bc9722ed82
Robin Yang <yyynyc@gmail.com>
2013-07-16 12:44:02 -0400

    changed post index to be ordered by created_at

--- app/models/grade.rb
+++
@@ -1,6 +0,0 @@
-class Grade < ActiveRecord::Base
-  attr_accessible :number, :name
-  has_many :domains
-  has_many :standards, through: :domains
-  has_many :posts, through: [:domains, :standards]
-end

2d0d5061463c10499f2eb74006cfdf2ec9a56632
Robin Yang <yyynyc@gmail.com>
2013-07-31 12:11:40 -0400

    added parental consent email upon user complete profile

--- app/models/state.rb
+++ app/models/state.rb
@@ -5,5 +5,8 @@ class State < ActiveRecord::Base
   after_create do
   	Event.create!(benefactor_id: self.user_id, beneficiary_id: 1, 
       event: "complete user profile", value: ShoolooV2::PROFILE_COMPLETE)
+  	if self.user.role == "student"
+  		UserMailer.parental_consent(self.user).deliver
+  	end
   end
 end

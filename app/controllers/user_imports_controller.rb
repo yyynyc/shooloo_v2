5400cf3fbef315f026540110f2446a3c202f357d
tesser <brenton@tentaclon.com>
2013-11-02 16:08:41 -0400

    Modify UserImport#load_imported_users to return an Array of User objects

--- app/controllers/user_imports_controller.rb
+++ app/controllers/user_imports_controller.rb
@@ -1,14 +1,14 @@
 class UserImportsController < ApplicationController
-	def new
-	    @user_import = UserImport.new
-	end
+  def new
+    @user_import = UserImport.new
+  end
 
-	def create
-	    @user_import = UserImport.new(params[:user_import])
-	    if @user_import.save
-	      redirect_to authorizations_path, notice: "Imported students successfully."
-	    else
-	      render :new
-	    end
-	end
+  def create
+    @user_import = UserImport.new(params[:user_import])
+    if @user_import.save
+      redirect_to authorizations_path, notice: "Imported students successfully."
+    else
+      render :new
+    end
+  end
 end
--- app/models/user_import.rb
+++ app/models/user_import.rb
@@ -1,54 +1,57 @@
 class UserImport 
-	extend ActiveModel::Naming
-	include ActiveModel::Conversion
-	include	ActiveModel::Validations
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
 
-	attr_accessor :file
+  attr_accessor :file
 
-	def initialize(attributes = {})
-		attributes.each { |name, value| send("#{name}=", value)}
-	end
+  def initialize(attributes = {})
+    attributes.each { |name, value| send("#{name}=", value)}
+  end
 
-	def persisted?
-		false
-	end
+  def persisted?
+    false
+  end
 
-	def save
-		if imported_users.map(&:valid?).all?
-			imported_users.each(&:save!)
-			true
-		else
-			imported_users.each_with_index do |user, index|
-				user.errors.full_messages.each do |message|
-					errors.add :base, "Row #{index+2}: #{message}"
-				end
-			end
-			false
-		end
-	end
+  def save
+    if imported_users.map(&:valid?).all?
+      imported_users.each(&:save!)
+      return true
+    end
+    imported_users.each_with_index do |user, index|
+      user.errors.full_messages.each do |message|
+        errors.add :base, "Row #{index+2}: #{message}"
+      end
+    end
+    return false
+  end
 
-	def imported_users
-		@imported_users ||= load_imported_users
-	end
+  def imported_users
+    @imported_users ||= load_imported_users
+  end
 
-	def load_imported_users 
-		spreadsheet = open_spreadsheet
-		header = spreadsheet.row(1)
-		(2..spreadsheet.last_row).each do |i|
-		    row = Hash[[header, spreadsheet.row(i)].transpose]
-		    user = User.new
-		    user.attributes = row.to_hash.slice(*User.accessible_attributes)
-		    user.save!
-		    Authorization.create!(authorized_id: user.id, authorizer_id: 1, approval: "accepted")
-  		end
-	end
+  def load_imported_users 
+    spreadsheet = open_spreadsheet
+    header = spreadsheet.row(1)
+    imported_users = []
+    (2..spreadsheet.last_row).each do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      user = User.new
+      user.attributes = row.to_hash.slice(*User.accessible_attributes)
+      user.save!
+      imported_users << user
+      Authorization.create!(authorized_id: user.id, authorizer_id: 1, approval: "accepted")
+    end
+    # Need to return an array of users not 2..2
+    imported_users
+  end
 
-	def open_spreadsheet
-		case File.extname(file.original_filename)
-		when '.csv' then Roo::Csv.new(file.path, nil, :ignore)
-		when '.xls' then Roo::Excel.new(file.path, nil, :ignore)
-		when '.xlsx' then Roo::Excelx.new(file.path, nil, :ignore)
-		else raise "Unknown file type: #{file.original_filename}"
-		end
-	end
+  def open_spreadsheet
+    case File.extname(file.original_filename)
+      when '.csv' then Roo::Csv.new(file.path, nil, :ignore)
+      when '.xls' then Roo::Excel.new(file.path, nil, :ignore)
+      when '.xlsx' then Roo::Excelx.new(file.path, nil, :ignore)
+      else raise "Unknown file type: #{file.original_filename}"
+    end
+  end
 end

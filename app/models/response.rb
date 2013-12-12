2985866ac78c507c8b03bfbf64e30d03a3ea279b
Robin Yang <yyynyc@gmail.com>
2013-12-12 14:53:26 -0500

    fixed bug in response.rb

--- app/models/response.rb
+++ app/models/response.rb
@@ -20,7 +20,11 @@ class Response < ActiveRecord::Base
     if self.gradings.any?
       self.marks.max.full_mark
     elsif self.comments.any?
-      self.comments.map(&:mark).sort_by(&:mark).last.full_mark
+      if self.comments.map(&:mark).any?
+        self.comments.map(&:mark).sort_by(&:mark).last.full_mark
+      else
+        return 0
+      end
     else      
       return 0
     end

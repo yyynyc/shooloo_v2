20481a21b6b40587b27180468e869aff9cc6e77b
Robin Yang <yyynyc@gmail.com>
2013-12-13 14:02:06 -0500

    added editing function to keeps

--- app/controllers/keeps_controller.rb
+++ app/controllers/keeps_controller.rb
@@ -19,6 +19,33 @@ class KeepsController < ApplicationController
     end
 	end
 
+  def edit
+    if current_user.admin? 
+      @keep = Keep.find(params[:id])
+    else
+      @keep = current_user.keeps.find_by_id(params[:id])
+    end 
+    @post = @keep.kept_post   
+  end
+
+  def update
+    if current_user.admin? 
+      @keep = Keep.find(params[:id])
+    else
+      @keep = current_user.keeps.find_by_id(params[:id])
+    end 
+    @post = @keep.kept_post 
+    if @keep.update_attributes(params[:keep])
+      flash[:success] = "You have upddated your save note successfully!"
+      redirect_to keeps_user_path(current_user)
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
+    @keep.destroy
+    flash[:success] = "Post removed from saved folder."
+    redirect_to keeps_user_path(current_user)
   end
 end
